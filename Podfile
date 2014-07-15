959da489081d565313b075768038f030f58332fd
David Crow <jdavidcrow@gmail.com>
2014-07-15 15:41:36 -0700

    added new file tree

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, :deployment_target => '5.0'
+
+
+pod 'DPMeterView', :local => './Pods'
+pod 'iRate', '~> 1.7.5'
+pod 'InAppSettingsKit', '~> 1.1'
+pod 'NMRangeSlider', '~> 1.0.1'
---
+++ Pods/Pods-header.h
@@ -0,0 +1,7 @@
+// WARNING: This feature of CocoaPods is present for discussion purposes and might be discontinued or changed in future
+#define __COCOA_PODS
+
+#define __POD_DPMeterView
+#define __POD_InAppSettingsKit
+#define __POD_NMRangeSlider
+#define __POD_iRate
